c918a815d269f91f4fe867aa86a48e21465cbbf9
Johannes Lauinger <johannes@lauinger-it.de>
2017-05-03 00:04:59 +0200

    properly correlate LTF, show nice diagrams, evaluate both xcorr methods

--- simulation/seemoo_correlate_preamble.m
+++ simulation/seemoo_correlate_preamble.m
@@ -5,7 +5,7 @@ SIGNAL = struct( ...
     'MOD_TYPE',           '80211g', ... % Signal type (kind of modulation / standard)
     'TYPE',               'DATA', ...   % Data frame
     'PAYLOAD',            randi([0 255], 1, 104), ...  % Custom payload data
-    'RATE',               2,  ...       % Modulation order (1-8)
+    'RATE',               1,  ...       % Modulation order (1-8)
     'SAMPLING_RATE',      40e6);        % Sampling rate of the signal
 
 % create signal
@@ -22,27 +22,46 @@ ltf_format = 'LTF';
     ieeeenc.create_preamble(stf_phase_shift, ltf_format);
 
 ltf_symbol_t = ltf_t_pre(193:320);
+stf_symbol_t = stf_t_pre(1:32);
 
 % results
 
 % introduce artificial delay, so that the packet does not begin right at
 % the start
-
-tx = [zeros(0, 1); tx];
+% also insert two more copies of the STF to match wl_example_siso plots :)
+tx = [tx(1:320); tx(1:320); tx];
 
 % correlate samples to find the LTF
 [acor, lag] = xcorr(tx, ltf_symbol_t);
-[~, I] = max(acor);
-delay = lag(I);
+[sts_acor, sts_lag] = xcorr(tx, stf_symbol_t);
 
 lts_corr = abs(conv(conj(fliplr(ltf_symbol_t)), sign(tx)));
+sts_corr = abs(conv(conj(fliplr(stf_symbol_t)), sign(tx)));
+
+[~, I] = max(lts_corr);
+delay = I;
+
+disp(delay-320*3); % subtract STF length
 
 % plot correlation values (probability of the preamble starting at this
 % frame) and show the estimated delay
 figure(1); clf; hold on;
-disp(delay-320); % subtract STF length
-l = length(acor);
-plot(lag, abs(acor), 'g');
-plot(lts_corr/35, 'b');
+title("LTF one-symbol correlation");
+plot(lag, abs(acor)*25, 'g');
+plot(lts_corr, '.-b', 'LineWidth', 1);
+ylims = ylim();
+line([960 960], [0 ylims(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+line([1320 1320], [0 ylims(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
 myAxis = axis();
 axis([1, 2000, myAxis(3), myAxis(4)])
+legend(["abs(xcorr(.,.))", "abs(conv(conj(fliplr(.)),sign(.)))", "LTF start", "LTF end"]);
+
+figure(2); clf; hold on;
+title("STF one-symbol correlation");
+plot(sts_lag, abs(sts_acor)*25, 'g');
+plot(sts_corr, '.-b', 'LineWidth', 1);
+ylims = ylim();
+line([960 960], [0 ylims(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+myAxis = axis();
+axis([1, 2000, myAxis(3), myAxis(4)])
+legend(["abs(xcorr(.,.))", "abs(conv(conj(fliplr(.)),sign(.)))", "STF end"]);

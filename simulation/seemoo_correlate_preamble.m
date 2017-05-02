7ed09415c42563714afcf90d3f1c34135eefeeb1
Johannes Lauinger <johannes@lauinger-it.de>
2017-05-02 18:07:42 +0200

    use only one LTF symbol for correlation, not the whole preamble

--- simulation/seemoo_correlate_preamble.m
+++ simulation/seemoo_correlate_preamble.m
@@ -1,4 +1,4 @@
-clear all; close all;
+%clear all; close all;
 
 % Signal generation settings IEEE 802.11g OFDM
 SIGNAL = struct( ...
@@ -21,42 +21,28 @@ ltf_format = 'LTF';
 [preamble, stf_t_pre, ltf_t_pre] = ...
     ieeeenc.create_preamble(stf_phase_shift, ltf_format);
 
+ltf_symbol_t = ltf_t_pre(193:320);
 
 % results
 
 % introduce artificial delay, so that the packet does not begin right at
 % the start
-tx = [zeros(242, 1); tx];
 
-% correlate samples to find the preamble
-[acor, lag] = xcorr(real(tx), real(preamble));
+tx = [zeros(0, 1); tx];
+
+% correlate samples to find the LTF
+[acor, lag] = xcorr(tx, ltf_symbol_t);
 [~, I] = max(acor);
 delay = lag(I);
 
+lts_corr = abs(conv(conj(fliplr(ltf_symbol_t)), sign(tx)));
+
 % plot correlation values (probability of the preamble starting at this
 % frame) and show the estimated delay
-disp(delay);
+figure(1); clf; hold on;
+disp(delay-320); % subtract STF length
 l = length(acor);
-plot(lag(ceil(l/2):end), acor(ceil(l/2):end));
-
-% try out complex correlation, similar to the one used in
-% ieee_80211_encoder
-
-% Complex cross correlation of Rx waveform with time-domain LTF
-ltf_corr = abs(conv(conj(fliplr(preamble)), sign(tx)));
-
-% Strip samples introduced by the convolution:
-ltf_corr = ltf_corr(1 + (length(preamble) / 2) : end - (length(preamble) / 2));
-
-figure(2);
-plot(ltf_corr);
-
-% Find all correlation peaks
-ltf_peaks = find(ltf_corr > 0.3 * max(ltf_corr));
-
-disp(ltf_peaks);
-% Select best candidate correlation peak as LTF-payload boundary
-[LTF1, LTF2] = meshgrid(ltf_peaks, ltf_peaks);
-[ltf_second_peak_index, ltf_first_peak_index] = find(LTF2 - LTF1 == length(preamble));
-
-disp(ltf_second_peak_index);
+plot(lag, abs(acor), 'g');
+plot(lts_corr/35, 'b');
+myAxis = axis();
+axis([1, 2000, myAxis(3), myAxis(4)])

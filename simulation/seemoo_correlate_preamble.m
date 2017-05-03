3c97de581698c87a2cb1bf2de8e399e6385a8955
Johannes Lauinger <johannes@lauinger-it.de>
2017-05-03 18:52:51 +0200

    use meshgrid to identify packet start based on all LTF peaks

--- simulation/seemoo_correlate_preamble.m
+++ simulation/seemoo_correlate_preamble.m
@@ -1,4 +1,11 @@
-clear all; close all;
+%clear all; close all;
+
+% relative correlation threshold to identify the LTF
+LTF_CORR_THRESHOLD = 0.8;
+
+% artificially introduced delay (zero samples)
+DELAY = 742;
+
 
 % Signal generation settings IEEE 802.11g OFDM
 SIGNAL = struct( ...
@@ -25,8 +32,7 @@ stf_symbol_t = stf_t_pre(1:32);
 
 % introduce artificial delay, so that the packet does not begin right at
 % the start
-% also insert two more copies of the STF to match wl_example_siso plots :)
-tx = [zeros(0, 1); tx(1:320); tx(1:320); tx];
+tx = [zeros(DELAY, 1); tx];
 
 % correlate samples to find the LTF
 [full_ltf_corr, full_ltf_lag] = xcorr(tx, ltf_symbol_t);
@@ -38,29 +44,58 @@ stf_corr = full_stf_corr(ceil(length(full_stf_corr)/2):end);
 ltf_lag = full_ltf_lag(ceil(length(full_ltf_lag)/2):end);
 stf_lag = full_ltf_lag(ceil(length(full_stf_lag)/2):end);
 
-%[~, I] = max(ltf_corr);
-%delay = I;
-%disp(delay-320*3); % subtract STF length
+
+% try to locate the packet start by finding the LTF peaks
+
+% Find all correlation peaks
+ltf_peaks = find(ltf_corr > LTF_CORR_THRESHOLD*max(ltf_corr));
+
+% Select best candidate correlation peak as LTS-payload boundary
+[LTF1, LTF2] = meshgrid(ltf_peaks, ltf_peaks);
+[ltf_second_peak_index, y] = find(LTF2-LTF1 == length(ltf_symbol_t));
+
+% calculate estimated indices
+ind.payload = ltf_peaks(max(ltf_second_peak_index)) + 128; % add 128 samples for the symbol itself
+ind.ltf = ind.payload - 320; % subtract LTF length
+ind.stf = ind.ltf - 320; % subtract STF length
+
 
 % plot correlation values (probability of the preamble starting at this
 % frame) and show the estimated delay
 figure(1); clf; hold on;
-title("LTF one-symbol correlation");
+title("LTF correlation and packet indices");
 plot(ltf_lag, abs(ltf_corr), '.-b', 'LineWidth', 1);
 myYlim = ylim();
-line([960 960], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
-line([1280 1280], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
-line([0 0], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+myXlim = xlim();
+line([myXlim(1) myXlim(2)], [LTF_CORR_THRESHOLD*abs(max(ltf_corr)) LTF_CORR_THRESHOLD*abs(max(ltf_corr))], 'LineStyle', '--', 'Color', 'g', 'LineWidth', 2);
+line([ind.stf ind.stf], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+line([ind.ltf ind.ltf], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+line([ind.payload ind.payload], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
 myAxis = axis();
-axis([-50, 2000, myAxis(3), myAxis(4)])
-legend(["abs(xcorr(.,.))", "LTF start", "LTF end", "Packet start"]);
+axis([0, ind.payload+400, myAxis(3), myAxis(4)])
+legend(["abs(xcorr(.,.))", "LTF correlation threshold", "Packet/STF start", "LTF start", "SIG start"]);
 
 figure(2); clf; hold on;
-title("STF one-symbol correlation");
+title("STF correlation and packet indices");
 plot(stf_lag, abs(stf_corr), '.-b', 'LineWidth', 1);
 myYlim = ylim();
-line([960 960], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
-line([0 0], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+line([ind.stf ind.stf], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+line([ind.ltf ind.ltf], [0 myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
 myAxis = axis();
-axis([-50, 2000, myAxis(3), myAxis(4)])
-legend(["abs(xcorr(.,.))", "STF end", "Packet start"]);
+axis([0, ind.payload+400, myAxis(3), myAxis(4)])
+legend(["abs(xcorr(.,.))", "Packet/STF start", "LTF start"]);
+
+figure(3); clf; hold on;
+title("Tx real waveform with estimated field indices");
+plot(real(tx), 'k');
+myYlim = ylim();
+line([ind.stf ind.stf], [myYlim(1) myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+line([ind.ltf ind.ltf], [myYlim(1) myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+line([ind.payload ind.payload], [myYlim(1) myYlim(2)], 'LineStyle', '--', 'Color', 'r', 'LineWidth', 2);
+myAxis = axis();
+axis([0, ind.payload+1000, myAxis(3), myAxis(4)])
+legend(["Tx", "Packet/STF start", "LTF start", "SIG start"]);
+
+
+% send some messages to the console
+fprintf(1, "Identified packet start after %d samples.\n", ind.stf-1);

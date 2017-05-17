a87d1f3e0bc82fbe610cc97f6848129fdc9e6798
Johannes Lauinger <johannes@lauinger-it.de>
2017-05-17 14:59:50 +0200

    use different plot type for scrambler variation

--- statistics/vary_scrambler.m
+++ statistics/vary_scrambler.m
@@ -19,11 +19,13 @@ NUM_ADDRESSES_TO_USE = 5;
 % number of experiments (choose sender randomly each time)
 NUM_EXPERIMENTS = 10;
 
+% MCS to use
+RATE = 0;
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-timing = zeros(8, 127);
-num_correct = zeros(8, 127);
+results = zeros(127, 3);
 
 file = fopen(filename_macs);
 out = textscan(file, "%s");
@@ -31,40 +33,32 @@ macs = cell2mat(out{1});
 macs = macs(:, [1:2 4:5 7:8 10:11 13:14 16:17]);
 macs = macs(1:NUM_ADDRESSES_TO_USE, :);
 
-for order = 0:7
-    order_time = tic;
-    for scrambler = 1:127
+for scrambler = 1:127
+    scrambler_time = tic;
+    for ex = 1:NUM_EXPERIMENTS
         % Note: it is possible that both senders are the same MAC here
         senders = macs(ceil(rand(2,1).*size(macs,1)),:);
         probe = struct( ...
             'duration', 'FFFF', ...
             'scrambler', scrambler);
-        tic;
-        evalc('guesses = find_sender(probe, order, macs, senders);');
-        timing(order+1,scrambler) = toc;
-        num_correct(order+1, scrambler) = correct_guesses(guesses, senders);
+        evalc('guesses = find_sender(probe, RATE, macs, senders);');
+        nc = correct_guesses(guesses, senders);
+        results(scrambler, nc+1) = results(scrambler, nc+1) + 1;
     end
-    fprintf(1, "INFO: done with MCS %d in %fs\n", order, toc(order_time));
+    fprintf(1, "INFO: done with Scrambler init %d in %fs\n", scrambler, toc(scrambler_time));
 end
 
 
 figure(1);
-plot(repmat(1:127, 8, 1)', num_correct');
-title(sprintf("Correct guesses for different MCS and Scrambler\n(out of %d addresses, after %d experiments)", NUM_ADDRESSES_TO_USE, NUM_EXPERIMENTS));
-xlabel("Scrambler Init");
-saveas(gcf, sprintf('figures/vary_scrambler-num_correct-%d_addresses.fig', NUM_ADDRESSES_TO_USE));
+bar(results, 'stacked');
+xlabel("Scrambler init");
+ylabel("# experiments");
+legend("0 correct", "1 correct", "2 correct");
+title(sprintf("Scrambler init at MCS %d\n(out of %d addresses, for %d experiments)", RATE, NUM_ADDRESSES_TO_USE, NUM_EXPERIMENTS));
+saveas(gcf, sprintf('figures/vary_scrambler-mcs_%d-%d_addresses-%d_experiments.fig', RATE, NUM_ADDRESSES_TO_USE, NUM_EXPERIMENTS));
 
-figure(2);
-plot(repmat(1:127, 8, 1)', timing');
-title("Time spent on correlating all possible MACs for different MCS and Scrambler");
-xlabel("MCS");
-saveas(gcf, sprintf('figures/vary_scrambler-timing-%d_addresses.fig', NUM_ADDRESSES_TO_USE));
-
-
-csvwrite(sprintf('results/vary_scrambler-num_correct-%d_addresses.csv', NUM_ADDRESSES_TO_USE), ...
-    num_correct);
-csvwrite(sprintf('results/vary_scrambler-timing-%d_addresses.csv', NUM_ADDRESSES_TO_USE), ...
-    timing);
+%csvwrite(sprintf('results/vary_scrambler-num_correct-%d_addresses.csv', NUM_ADDRESSES_TO_USE), ...
+%    num_correct);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
